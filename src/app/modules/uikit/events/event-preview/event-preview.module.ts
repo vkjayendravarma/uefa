f541bc2bb6d3f594d51077587b5ad7909e94d007
Pradeep Chagarlamudi <pradeep.speck@gmail.com>
2021-08-19 11:23:02 +0530

    Event preview page is added at the route /dashboard/admin/events/preview.

--- src/app/modules/uikit/events/event-preview/event-preview.module.ts
+++ src/app/modules/uikit/events/event-preview/event-preview.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventPreviewComponent } from './event-preview.component';
 import { HeaderModule } from '../../header/header.module';
+import { MatButtonModule } from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
 
 
 
@@ -11,7 +13,9 @@ import { HeaderModule } from '../../header/header.module';
   ],
   imports: [
     CommonModule,
-    HeaderModule
+    HeaderModule,
+    MatButtonModule,
+    MatChipsModule
   ],
   exports: [EventPreviewComponent]
 })
