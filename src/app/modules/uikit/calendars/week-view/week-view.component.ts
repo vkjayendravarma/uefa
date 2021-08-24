4c0e135773c6f83a4c3f3f5ac549cc877de4d4d7
Asu Kaur <asukaur99@gmail.com>
2021-08-24 16:07:16 +0530

    -

--- src/app/modules/uikit/calendars/week-view/week-view.component.ts
+++ src/app/modules/uikit/calendars/week-view/week-view.component.ts
@@ -3,13 +3,133 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'uikit-calendar-week-view',
   templateUrl: './week-view.component.html',
-  styleUrls: ['./week-view.component.scss']
+  styleUrls: ['./week-view.component.scss'],
 })
 export class WeekViewComponent implements OnInit {
+  header = [
+    { day: 'SUN', date: '21' },
+    { day: 'SUN', date: '21' },
+    { day: 'SUN', date: '21' },
+    { day: 'SUN', date: '21' },
+    { day: 'SUN', date: '21' },
+  ];
 
-  constructor() { }
+  task = [
+    {
+      done: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+      notDone: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+    },
+    {
+      done: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+      notDone: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+    },
+    {
+      done: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+      notDone: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+    },
+    {
+      done: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+      notDone: [],
+    },
+    {
+      done: [],
+      notDone: [
+        {
+          name: 'Task 1',
+          time: '10:00AM - 12:00PM',
+        },
+      ],
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
