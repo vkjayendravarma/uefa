ebd25cee0bb54c8a5bf7a7d26042dc62f9f15d89
Asu Kaur <asukaur99@gmail.com>
2021-08-24 19:07:50 +0530

    Added demo data and for loop

--- src/app/modules/uikit/calendars/week-view/week-view.component.ts
+++ src/app/modules/uikit/calendars/week-view/week-view.component.ts
@@ -6,12 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./week-view.component.scss'],
 })
 export class WeekViewComponent implements OnInit {
+  // Getting dates
+  date = new Date();
+  firstDay = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
+  lastDay = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0);
+
+  // Demo Data
   header = [
     { day: 'SUN', date: '21' },
-    { day: 'SUN', date: '21' },
-    { day: 'SUN', date: '21' },
-    { day: 'SUN', date: '21' },
-    { day: 'SUN', date: '21' },
+    { day: 'SUN', date: '22' },
+    { day: 'SUN', date: '23' },
+    { day: 'SUN', date: '24' },
+    { day: 'SUN', date: '25' },
   ];
 
   task = [
@@ -54,12 +60,7 @@ export class WeekViewComponent implements OnInit {
       ],
     },
     {
-      done: [
-        {
-          name: 'Task 1',
-          time: '10:00AM - 12:00PM',
-        },
-      ],
+      done: [],
       notDone: [
         {
           name: 'Task 1',
