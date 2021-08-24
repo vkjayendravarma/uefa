cab886e8dd7b064bc2e501701b42d064111805ec
Keshav Varma <vkjvarma@gmail.com>
2021-08-24 23:25:46 +0530

    login signup

---
+++ src/app/modules/admin/pages/surveys/a-new-survey/a-new-survey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a-new-survey',
+  templateUrl: './a-new-survey.component.html',
+  styleUrls: ['./a-new-survey.component.scss']
+})
+export class ANewSurveyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/uikit/surveys/create-new-survey/create-new-survey.module.ts
+++ src/app/modules/uikit/surveys/create-new-survey/create-new-survey.module.ts
@@ -1,12 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SurveyDetailsComponent } from './components/survey-details/survey-details.component';
+import { SurveyPreviewComponent } from './components/survey-preview/survey-preview.component';
+import { SurveyPublishComponent } from './components/survey-publish/survey-publish.component';
+import { SurveyResponcesComponent } from './components/survey-responces/survey-responces.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatChipsModule } from '@angular/material/chips';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SurveyDetailsComponent, SurveyPreviewComponent, SurveyPublishComponent, SurveyResponcesComponent, CreateNewSurveyModule],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatChipsModule,
+  ],
+  exports: [CreateNewSurveyModule]
 })
 export class CreateNewSurveyModule { }
