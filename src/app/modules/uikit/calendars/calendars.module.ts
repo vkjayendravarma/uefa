b3348ce94375d641a6a464ca7c86f891b1202a99
Pradeep Chagarlamudi <pradeep.speck@gmail.com>
2021-08-25 13:08:44 +0530

    Month view calendar is added at /dashboard/player/home.

--- src/app/modules/uikit/calendars/calendars.module.ts
+++ src/app/modules/uikit/calendars/calendars.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MonthViewComponent } from './month-view/month-view.component';
 import { WeekViewComponent } from './week-view/week-view.component';
+import { DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarModule } from 'angular-calendar';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -11,7 +16,10 @@ import { WeekViewComponent } from './week-view/week-view.component';
     WeekViewComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   exports: [MonthViewComponent, WeekViewComponent]
 })
