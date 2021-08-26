87e0f51d223e5ad69ff68a86d04114a2755672fe
Keshav Varma <vkjvarma@gmail.com>
2021-08-26 23:48:11 +0530

    profile component

---
+++ src/app/modules/admin/pages/a-profile/a-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a-profile',
+  templateUrl: './a-profile.component.html',
+  styleUrls: ['./a-profile.component.scss']
+})
+export class AProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
