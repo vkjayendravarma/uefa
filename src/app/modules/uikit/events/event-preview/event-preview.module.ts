533b16f248e3378e5b9d623c94f1dd085dd14170
Asu Kaur <asukaur99@gmail.com>
2021-08-27 13:18:13 +0530

    Event Preview page

--- src/app/modules/uikit/events/event-preview/event-preview.module.ts
+++ src/app/modules/uikit/events/event-preview/event-preview.module.ts
@@ -2,17 +2,44 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventPreviewComponent } from './event-preview.component';
 import { HeaderModule } from '../../header/header.module';
-
-
+import { TagChipsModule } from '../../global/tag-chips/tag-chips.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormUiElementsModule } from '../../form-ui-elements/form-ui-elements.module';
+import { RouterModule } from '@angular/router';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  declarations: [
-    EventPreviewComponent
-  ],
+  declarations: [EventPreviewComponent],
   imports: [
     CommonModule,
-    HeaderModule
+    HeaderModule,
+    TagChipsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatStepperModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    FormUiElementsModule,
+    RouterModule,
+    MatChipsModule,
+    MatRadioModule,
   ],
-  exports: [EventPreviewComponent]
+  exports: [EventPreviewComponent],
 })
-export class EventPreviewModule { }
+export class EventPreviewModule {}
