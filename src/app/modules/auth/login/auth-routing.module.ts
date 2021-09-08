f7050d7faa295b854c953e614039788e5a48038c
Keshav Varma <vkjvarma@gmail.com>
2021-09-08 18:34:06 +0530

    login signup links

--- src/app/modules/auth/login/auth-routing.module.ts
+++ src/app/modules/auth/login/auth-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            redirectTo: 'session/:authType',
+            redirectTo: 'session/login',
             pathMatch: 'full'
           },
           {
