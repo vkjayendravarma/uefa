4215a49d4ce2df8080b4874596b443510dca72f3
Asu Kaur <asukaur99@gmail.com>
2021-09-10 18:22:36 +0530

    Team tabs responsiveness

--- src/app/modules/players/players.module.ts
+++ src/app/modules/players/players.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PDashboardComponent } from './pages/p-dashboard/p-dashboard.component';
 import { CalendarsModule } from '../uikit/calendars/calendars.module';
 import { PlayerRouterModule } from './players-routing.module';
-
-
+import { HeaderModule } from '../uikit/header/header.module';
 
 @NgModule({
-  declarations: [
-    PDashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    PlayerRouterModule,
-    CalendarsModule
-  ]
+  declarations: [PDashboardComponent],
+  imports: [CommonModule, PlayerRouterModule, CalendarsModule, HeaderModule],
 })
-export class PlayersModule { }
+export class PlayersModule {}
