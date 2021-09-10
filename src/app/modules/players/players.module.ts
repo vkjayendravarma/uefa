7ef778ae989bae49a4aaddc198a3017a80bf0b8b
Asu Kaur <asukaur99@gmail.com>
2021-09-11 03:06:26 +0530

    Created Players Home page

--- src/app/modules/players/players.module.ts
+++ src/app/modules/players/players.module.ts
@@ -4,9 +4,22 @@ import { PDashboardComponent } from './pages/p-dashboard/p-dashboard.component';
 import { CalendarsModule } from '../uikit/calendars/calendars.module';
 import { PlayerRouterModule } from './players-routing.module';
 import { HeaderModule } from '../uikit/header/header.module';
+import { DrillsSessionsModule } from '../uikit/drills-sessions/drills-sessions.module';
+import { EventsUiKitModule } from '../uikit/events/events-ui-kit/events-ui-kit.module';
+import { TeamsModule } from '../uikit/teams/teams.module';
+import { ViewPostsModule } from '../uikit/posts/view-posts/view-posts.module';
 
 @NgModule({
   declarations: [PDashboardComponent],
-  imports: [CommonModule, PlayerRouterModule, CalendarsModule, HeaderModule],
+  imports: [
+    CommonModule,
+    PlayerRouterModule,
+    CalendarsModule,
+    HeaderModule,
+    EventsUiKitModule,
+    DrillsSessionsModule,
+    TeamsModule,
+    ViewPostsModule,
+  ],
 })
 export class PlayersModule {}
