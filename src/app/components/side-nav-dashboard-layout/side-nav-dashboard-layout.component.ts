f6851e8ff9c98856f8f069ff41b85a9aeae76365
Asu Kaur <asukaur99@gmail.com>
2021-09-11 19:05:49 +0530

    Added new routes for players

--- src/app/components/side-nav-dashboard-layout/side-nav-dashboard-layout.component.ts
+++ src/app/components/side-nav-dashboard-layout/side-nav-dashboard-layout.component.ts
@@ -8,6 +8,7 @@ import { SideBarItemsPrototype } from 'src/app/prototypes/sidebarItems.interface
 })
 export class SideNavDashboardLayoutComponent implements OnInit {
   currentSideBar: SideBarItemsPrototype[] = [];
+  currentSideBar2: SideBarItemsPrototype[] = [];
   user = 'facilitator';
 
   sidebarAdmin: SideBarItemsPrototype[] = [
@@ -98,7 +99,6 @@ export class SideNavDashboardLayoutComponent implements OnInit {
       link: '/dashboard/player/posts',
       icon: 'posts',
     },
-
     {
       label: 'Surveys',
       link: '/dashboard/player/surveys',
@@ -111,7 +111,7 @@ export class SideNavDashboardLayoutComponent implements OnInit {
     },
   ];
 
-  sideBar2: SideBarItemsPrototype[] = [
+  sideBar2Admin: SideBarItemsPrototype[] = [
     {
       label: 'Notifications',
       link: '/notifications',
@@ -120,6 +120,41 @@ export class SideNavDashboardLayoutComponent implements OnInit {
     {
       label: 'Manage Users',
       link: '/dashboard/admin/users',
+      icon: 'user',
+    },
+    {
+      label: 'Logout',
+      link: '/auth/session/login',
+      icon: 'logout',
+    },
+  ];
+
+  sideBar2Facilitator: SideBarItemsPrototype[] = [
+    {
+      label: 'Notifications',
+      link: '/notifications',
+      icon: 'notifications',
+    },
+    {
+      label: 'Manage Users',
+      link: '/dashboard/facilitator',
+      icon: 'user',
+    },
+    {
+      label: 'Logout',
+      link: '/auth/session/login',
+      icon: 'logout',
+    },
+  ];
+  sideBar2Player: SideBarItemsPrototype[] = [
+    {
+      label: 'Notifications',
+      link: '/notifications',
+      icon: 'notifications',
+    },
+    {
+      label: 'Manage Users',
+      link: '/dashboard/players/teams',
       icon: 'user',
     },
     {
@@ -139,14 +174,18 @@ export class SideNavDashboardLayoutComponent implements OnInit {
     let currentUserRole = window.localStorage.getItem('role');
     if (currentUserRole == 'admin') {
       this.currentSideBar = this.sidebarAdmin;
+      this.currentSideBar2 = this.sideBar2Admin;
       return;
     }
     if (currentUserRole == 'facilitator') {
       this.currentSideBar = this.sidebarFacilitators;
+      this.currentSideBar2 = this.sideBar2Facilitator;
       return;
     }
     if (currentUserRole == 'player') {
       this.currentSideBar = this.sidebarPlayers;
+      this.currentSideBar2 = this.sideBar2Player;
+
       return;
     }
   }
--- src/app/modules/players/players-routing.module.ts
+++ src/app/modules/players/players-routing.module.ts
@@ -12,7 +12,95 @@ const routes: Routes = [
     path: 'home',
     component: PDashboardComponent,
   },
-  
+
+  // {
+  //   path: 'surveys',
+  //   component: FSurveysComponent,
+  // },
+  // {
+  //   path: 'posts',
+  //   component: FPostsComponent}
+
+  //    {
+  //   path: 'events',
+  //   component: FEventsComponent},
+  //    {
+  //   path: 'tournaments',
+  //   component: FTournamentsComponent},
+
+  {
+    path: 'teams',
+    // component: FTeamsComponent,
+    children: [
+      {
+        path: '',
+        // component: FTeamsDashboardComponent,
+      },
+      {
+        path: 'team',
+        // component: FTeamViewComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'home',
+          },
+          {
+            path: 'home',
+            // component: FTeamManagementHomeComponent,
+          },
+          {
+            path: 'members',
+            // component: FTeamManagementMembersComponent,
+            children: [
+              {
+                path: 'details/:id',
+                // component: TeamMemberDetailsComponent,
+              },
+              {
+                path: 'details/:id/edit',
+                // component: TeamMemberDetailsEditComponent,
+              },
+            ],
+          },
+          {
+            path: 'sessions',
+            children: [
+              {
+                path: '',
+                // component: FTeamManagementSessionsComponent,
+              },
+              {
+                path: 'invites/:id',
+                // component: TeamSessionManageMembersComponent,
+              },
+            ],
+          },
+          {
+            path: 'documents',
+            // component: FTeamManagementDocumentsComponent,
+          },
+          {
+            path: 'squads',
+            // component: FTeamManagementSquadsComponent,
+            children: [
+              {
+                path: 'details/:id',
+                // component: TeamSquadComponent,
+              },
+            ],
+          },
+          {
+            path: 'attendance',
+            // component: FTeamManagementAttendanceComponent,
+          },
+          {
+            path: 'announcements',
+            // component: FTeamManagementAnnouncementsComponent,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 @NgModule({
