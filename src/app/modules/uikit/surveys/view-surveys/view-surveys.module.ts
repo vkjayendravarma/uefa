b80dae2633a1b74cda620cbe8f106727da21b6ba
Keshav Varma <vkjvarma@gmail.com>
2021-09-12 20:34:53 +0530

    home page components header dynamic

--- src/app/modules/uikit/surveys/view-surveys/view-surveys.module.ts
+++ src/app/modules/uikit/surveys/view-surveys/view-surveys.module.ts
@@ -4,7 +4,6 @@ import { SurveysComponent } from './surveys/surveys.component';
 import { MatIconModule } from '@angular/material/icon';
 import { TagChipsModule } from '../../global/tag-chips/tag-chips.module';
 import { SureveysElementComponent } from './sureveys-element/sureveys-element.component';
-import { SureveysElementNewComponent } from './sureveys-elementnew/sureveys-elementnew.component';
 import { SurveysTabViewComponent } from './surveys-tab-view/surveys-tab-view.component';
 import { MatTabsModule } from '@angular/material/tabs';
 
@@ -12,7 +11,6 @@ import { MatTabsModule } from '@angular/material/tabs';
   declarations: [
     SurveysComponent,
     SureveysElementComponent,
-    SureveysElementNewComponent,
     SurveysTabViewComponent,
   ],
   imports: [CommonModule, MatIconModule, TagChipsModule, MatTabsModule],
