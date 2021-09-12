d6fbda5796ad70e7adbb5d808ff3c5d36817b0b3
Keshav Varma <vkjvarma@gmail.com>
2021-09-12 18:06:40 +0530

    conflict if condition team management

--- src/app/modules/uikit/calendars/calendars.module.ts
+++ src/app/modules/uikit/calendars/calendars.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MonthViewComponent } from './month-view/month-view.component';
 import { WeekViewComponent } from './week-view/week-view.component';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarModule } from 'angular-calendar';
 
 @NgModule({
   declarations: [MonthViewComponent, WeekViewComponent],
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule,
+    MatIconModule,
+    RouterModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),],
   exports: [MonthViewComponent, WeekViewComponent],
 })
 export class CalendarsModule {}
