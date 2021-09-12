5b2791f62e3b2702d2b6918a29ced4e1b90564d8
Kirtika Malhotra <malhotra.kirtika@gmail.com>
2021-09-13 00:48:16 +0530

    people screens created

--- src/app/modules/facilitators/components/people/f-people-players/f-people-players.component.ts
+++ src/app/modules/facilitators/components/people/f-people-players/f-people-players.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-f-people-players',
   templateUrl: './f-people-players.component.html',
-  styleUrls: ['./f-people-players.component.scss']
+  styleUrls: ['./f-people-players.component.scss'],
 })
 export class FPeoplePlayersComponent implements OnInit {
+  Players = [
+    {
+      name: 'Mo Salah',
+      email: 'abc@gmail.com',
+      teams: ['Men U15', 'Men U15'],
+    },
+    {
+      name: 'Mo Salah',
+      email: 'abc@gmail.com',
+      teams: ['Men U15', 'Men U15'],
+    },
+    {
+      name: 'Mo Salah',
+      email: 'abc@gmail.com',
+      teams: ['Men U15', 'Men U15'],
+    },
+    {
+      name: 'Mo Salah',
+      email: 'abc@gmail.com',
+      teams: ['Men U15', 'Men U15'],
+    },
+    {
+      name: 'Mo Salah',
+      email: 'abc@gmail.com',
+      teams: ['Men U15', 'Men U15'],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/facilitators/components/people/f-people-requests/f-people-requests.component.ts
+++ src/app/modules/facilitators/components/people/f-people-requests/f-people-requests.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-f-people-requests',
   templateUrl: './f-people-requests.component.html',
-  styleUrls: ['./f-people-requests.component.scss']
+  styleUrls: ['./f-people-requests.component.scss'],
 })
 export class FPeopleRequestsComponent implements OnInit {
+  Requests = [
+    {
+      email: 'abc@gmail.com',
+      role: 'Admin',
+    },
+    {
+      email: 'abc@gmail.com',
+      role: 'Admin',
+    },
+    {
+      email: 'abc@gmail.com',
+      role: 'Admin',
+    },
+    {
+      email: 'abc@gmail.com',
+      role: 'Admin',
+    },
+    {
+      email: 'abc@gmail.com',
+      role: 'Admin',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
