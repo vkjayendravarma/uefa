33fdf32e35541bbc79e43cc67ae4cf7002244e51
Asu Kaur <asukaur99@gmail.com>
2021-09-12 17:31:33 +0530

    Completed player events

--- src/app/modules/uikit/events/events-ui-kit/components/organized-view/organized-view.component.ts
+++ src/app/modules/uikit/events/events-ui-kit/components/organized-view/organized-view.component.ts
@@ -3,20 +3,22 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-organized-view',
   templateUrl: './organized-view.component.html',
-  styleUrls: ['./organized-view.component.scss']
+  styleUrls: ['./organized-view.component.scss'],
 })
-export class OrganizedViewComponent  implements OnInit  {
-
+export class OrganizedViewComponent implements OnInit {
   @Input()
   componentTitle!: string;
 
   @Input()
+  role: string | undefined;
+
+  @Input()
   enableCreateNew: boolean = false;
-  constructor(  ) {
-    
-  }
 
-  ngOnInit(): void {
-  }
+  eventsList = ['Current', 'Upcoming', 'Drafts'];
+  eventsPlayer = ['Browse', 'Registered Events'];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
