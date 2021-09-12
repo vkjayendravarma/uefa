7f5b42c491ecb14a0aa1e8e53e2cde33ff14287c
Asu Kaur <asukaur99@gmail.com>
2021-09-13 00:52:53 +0530

    Added props to posts-list, stats-list, events component

--- src/app/modules/uikit/associations/stats-list-view/stats-list-view.component.ts
+++ src/app/modules/uikit/associations/stats-list-view/stats-list-view.component.ts
@@ -3,18 +3,30 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stats-list-view',
   templateUrl: './stats-list-view.component.html',
-  styleUrls: ['./stats-list-view.component.scss']
+  styleUrls: ['./stats-list-view.component.scss'],
 })
 export class StatsListViewComponent implements OnInit {
+  @Input()
+  isCompleted: boolean = false;
 
   @Input()
   componentTitle: string | undefined;
 
-  regestrations = [1,2,3,4,5,6]
+  @Input()
+  enableFilter: boolean = false;
 
-  constructor() { }
+  @Input()
+  enableAddNew: boolean = false;
 
-  ngOnInit(): void {
-  }
+  @Input()
+  enableSearch: boolean = false;
 
+  @Input()
+  textAction: string | undefined;
+
+  regestrations = [1, 2, 3, 4, 5, 6];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
