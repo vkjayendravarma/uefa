8e31bfdae7155fd00570bf630729fd75b48a356a
Asu Kaur <asukaur99@gmail.com>
2021-09-13 23:52:41 +0530

    Fixed signup success

--- src/app/modules/auth/login/player-signup-successful/signup-successful.component.ts
+++ src/app/modules/auth/login/player-signup-successful/signup-successful.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-signup-successful',
   templateUrl: './signup-successful.component.html',
-  styleUrls: ['./signup-successful.component.scss']
+  styleUrls: ['./signup-successful.component.scss'],
 })
 export class SignupSuccessfulComponent implements OnInit {
+  selected: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.selected = 1;
   }
 
+  ngOnInit(): void {}
+
+  setSelected(option: any) {
+    this.selected = option;
+    console.log(this.selected);
+  }
 }
