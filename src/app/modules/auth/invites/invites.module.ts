7d0ee1d0f30905ec0dfe505ac8952c68c9ed6081
Kirtika Malhotra <malhotra.kirtika@gmail.com>
2021-09-13 21:13:54 +0530

    created forgot password email template

--- src/app/modules/auth/invites/invites.module.ts
+++ src/app/modules/auth/invites/invites.module.ts
@@ -8,9 +8,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { EmailTemplateRegistrationApprovedComponent } from './email-template-registration-approved/email-template-registration-approved.component';
 import { InviteDependentToPlatformComponent } from './invite-dependent-to-platform/invite-dependent-to-platform.component';
 import { FEmailTemplateComponent } from './f-email-template/f-email-template.component';
+import { ResetPasswordEmailTemplateComponent } from './reset-password-email-template/reset-password-email-template.component';
 
 @NgModule({
-  declarations: [InviteToPlatformComponent, EmailTemplateInviteComponent, EmailTemplateRegistrationApprovedComponent, InviteDependentToPlatformComponent, FEmailTemplateComponent],
+  declarations: [InviteToPlatformComponent, EmailTemplateInviteComponent, EmailTemplateRegistrationApprovedComponent, InviteDependentToPlatformComponent, FEmailTemplateComponent, ResetPasswordEmailTemplateComponent],
   imports: [CommonModule, InvitesRouterModule, MatButtonModule, MatIconModule],
 })
 export class InvitesModule {}
---
+++ src/app/modules/auth/invites/reset-password-email-template/reset-password-email-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-email-template',
+  templateUrl: './reset-password-email-template.component.html',
+  styleUrls: ['./reset-password-email-template.component.scss']
+})
+export class ResetPasswordEmailTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
